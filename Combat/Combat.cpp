5fc4243fd31c0e46983af8454fed4f862139d87a
pieraqua <pauloffxii@hotmail.com>
2024-03-10 14:44:03 -0300

    Combate fluindo

--- Combat/Combat.cpp
+++ Combat/Combat.cpp
@@ -27,19 +27,29 @@ Combat::Combat(vector<Character*> allies, vector<Character*> enemies)
     this->enemies = enemies;
 }
 
-bool Combat::take_turn()
+int Combat::take_turn()
 {
     SystemOutput* output = SystemOutput::getInstance();
     SystemInput* input = SystemInput::getInstance();
     vector<string> user_action;
     vector<Character*>::iterator it;
+
+    // Retorna 2 se os inimigos ganharam
+    int end_combat_allies = 2;
+    // Retorna 1 se os aliados ganharam
+    int end_combat_enemies = 1;
+
     for (it = this->allies.begin(); it < this->allies.end(); it++)
     {
-        output->printToConsole("What will " + (*it)->get_name() + " do?\n\x1b[?12h\x1b[1mAttack\x1b[0m", SystemOutput::outputLevel::INFO);
-        
-        while (!handle_action(input->getUserInput(), (*it)))
-        {
-            output->printToConsole("Invalid command.\nWhat will " + (*it)->get_name() + " do?\n\x1b[?12h\x1b[1mAttack\x1b[0m", SystemOutput::outputLevel::INFO);
+        if ((*it)->get_alive()) {
+            output->printToConsole("What will " + (*it)->get_name() + " do?\n\x1b[?12h\x1b[1mAttack\x1b[0m", SystemOutput::outputLevel::INFO);
+
+            while (!handle_action(input->getUserInput(), (*it)))
+            {
+                output->printToConsole("Invalid command.\nWhat will " + (*it)->get_name() + " do?\n\x1b[?12h\x1b[1mAttack\x1b[0m", SystemOutput::outputLevel::INFO);
+            }
+
+            end_combat_allies = 0;
         }
 
     }
@@ -47,9 +57,36 @@ bool Combat::take_turn()
     for (it = this->enemies.begin(); it < this->enemies.end(); it++)
     {
         if ((*it)->get_alive())
-            return false;
+        {
+            if (!end_combat_allies) {
+                (*it)->attack(this->allies.at(Utils::rolldx(this->allies.size())));
+                output->printToConsole("Press enter to continue.", SystemOutput::outputLevel::INFO);
+                input->getUserInput();
+            }
+            end_combat_enemies = 0;
+        }
     }
-    return true;
+    
+
+    // Retorna 3 se ambos tiverem sido derrotados (empate)
+    this->combat_in_progress = (end_combat_allies | end_combat_enemies);
+
+    switch (this->combat_in_progress) 
+    {
+    case 1:
+        output->printToConsole("The enemies have been defeated!", SystemOutput::outputLevel::INFO);
+        break;
+    case 2:
+        output->printToConsole("The allies have been defeated!", SystemOutput::outputLevel::INFO);
+        break;
+    case 3:
+        output->printToConsole("Both parties have been defeated!", SystemOutput::outputLevel::INFO);
+        break;
+    default:
+        break;
+    }
+
+    return this->combat_in_progress;
 }
 
 void Combat::print_participants()
--- Combat/Combat.h
+++ Combat/Combat.h
@@ -10,13 +10,16 @@ class Combat
 	vector<Character*> allies;
 	vector<Character*> enemies;
 
-	bool combat_in_progress;
+	int combat_in_progress;
 	bool handle_action(vector<string>, Character*);
 
 public:
 	Combat(vector<Character*> allies, vector<Character*> enemies);
-	bool take_turn();
-	bool combat_finished() { return !combat_in_progress; }
+	int take_turn();
+
+	// Retorna 0 se combate ainda não acabou, 1 se os aliados ganharam e 2 se os inimigos ganharam.
+	int combat_finished() { return combat_in_progress; }
+
 
 	void print_participants();
 };
